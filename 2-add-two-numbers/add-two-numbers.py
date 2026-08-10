f71b595cf74b2c7d94408c44873dc1127c0c7b68
karnamkavya <karnamkavya1@gmail.com>
2026-08-10 12:35:41 +0530

    Time: 3 ms (64.97%) | Memory: 19.4 MB (40.80%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,30 +5,37 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-        dummy = ListNode(0)
-        current = dummy
-        carry = 0
-
-        while l1 or l2 or carry:
-            x = l1.val if l1 else 0
-            y = l2.val if l2 else 0
-
-            total = x + y + carry
-            carry = total // 10
-
-            current.next = ListNode(total % 10)
-            current = current.next
-
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
-
+        dummy=ListNode(0)
+        curr=dummy
+        carry=0
+        while l1 and l2:
+            v=l1.val+l2.val+carry
+            carry=v//10
+            v=v%10
+            curr.next=ListNode(v)
+            l1=l1.next
+            l2=l2.next
+            curr=curr.next
+        while l1:
+            if carry==1:
+                v=l1.val+carry
+                carry=v//10
+                v=v%10
+                curr.next=ListNode(v)
+            else:
+                curr.next=l1
+            l1=l1.next
+            curr=curr.next
+        while l2:
+            if carry==1:
+                v=l2.val+carry
+                carry=v//10
+                v=v%10
+                curr.next=ListNode(v)
+            else:
+                curr.next=l2
+            l2=l2.next
+            curr=curr.next  
+        if carry==1:
+            curr.next=ListNode(carry)      
         return dummy.next
-        
